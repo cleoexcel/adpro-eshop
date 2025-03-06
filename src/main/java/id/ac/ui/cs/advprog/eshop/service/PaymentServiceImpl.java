c739724362c7e36e0d9e4b392a54c05e7761a6ea
cleoexcel <cleoxclln@gmail.com>
2025-03-06 13:08:39 +0700

    [GREEN] Add interfaces and implementation of Payment service

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,46 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, PaymentStatus.SUCCESS.getValue(), paymentData, order);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        if (paymentRepository.findById(payment.getId()) == null) {
+            throw new NoSuchElementException();
+        }
+
+        payment.setStatus(status);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public Payment getPayment(String paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return paymentRepository.getAllPayments();
+    }
+}
