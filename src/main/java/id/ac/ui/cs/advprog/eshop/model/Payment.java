80a7d956f6298fe397396115a93abd866e2554c8
cleoexcel <cleoxclln@gmail.com>
2025-03-06 12:45:27 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -8,15 +8,45 @@ import java.util.Map;
 @Builder
 @Getter
 public class Payment {
-    private String id;
-    private String method;
-    private String status;
-    private Map<String, String> paymentData;
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
         this.id = id;
         this.method = method;
         this.status = status;
         this.paymentData = paymentData;
+
+        if (this.method.equals("BANK_TRANSFER")) {
+            if (!paymentData.containsKey("bankName")
+                    || !paymentData.containsKey("referenceCode")
+                    || paymentData.get("bankName") == null
+                    || paymentData.get("referenceCode") == null) {
+                this.status = "REJECTED";
+            }
+        } else if (this.method.equals("VOUCHER_CODE")) {
+            if (!paymentData.containsKey("voucherCode")
+                    || paymentData.get("voucherCode") == null
+                    || paymentData.get("voucherCode").length() != 16
+                    || !paymentData.get("voucherCode").startsWith("ESHOP")) {
+                this.status = "REJECTED";
+                return;
+            }
+
+            int numericalCount = 0;
+            for (int i=0; i<paymentData.get("voucherCode").length(); i++) {
+                char currentChar = paymentData.get("voucherCode").charAt(i);
+                if (currentChar >= '0' && currentChar <= '9') {
+                    numericalCount++;
+                }
+            }
+            if (numericalCount != 8) {
+                this.status = "REJECTED";
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
